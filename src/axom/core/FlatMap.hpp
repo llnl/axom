c1fecc47c3328bb3b28ee8eb2fd820921754bf27
Max Yang <yang39@llnl.gov>
2025-03-27 16:53:41 -0700

    FlatMap: fix preallocated bucket logic

--- src/axom/core/FlatMap.hpp
+++ src/axom/core/FlatMap.hpp
@@ -726,7 +726,7 @@ FlatMap<KeyType, ValueType, Hash>::FlatMap(IndexType bucket_count, Allocator all
   // TODO: we should add a countl_zero overload for 64-bit integers
   {
     std::int32_t numGroups = std::ceil((bucket_count + 1) / (double)BucketsPerGroup);
-    m_numGroups2 = 31 - (axom::utilities::countl_zero(numGroups));
+    m_numGroups2 = 32 - (axom::utilities::countl_zero(numGroups));
   }
 
   IndexType numGroupsRounded = 1 << m_numGroups2;
