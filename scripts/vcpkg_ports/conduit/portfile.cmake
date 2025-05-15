3b34d1ba94767f4a5bf44d04616713fcccc5900f
Kenny Weiss <kenny@kennyweiss.com>
2025-05-14 17:22:59 -0700

    Fixes absolute paths in conduit portfile

--- scripts/vcpkg_ports/conduit/portfile.cmake
+++ scripts/vcpkg_ports/conduit/portfile.cmake
@@ -32,9 +32,10 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake
-                          TARGET_PATH share
-                          TOOLS_PATH tools/conduit)
+vcpkg_cmake_config_fixup(
+        PACKAGE_NAME conduit
+        CONFIG_PATH  lib/cmake
+        TOOLS_PATH   tools/conduit)
 vcpkg_copy_pdbs()
 
 
