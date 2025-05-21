93193c0114c6da437b134d010dcf31e4b9897771
Brad Whitlock <whitlock2@llnl.gov>
2025-05-20 17:58:04 -0700

    Remove RAJA dependency in some quest examples

--- src/axom/quest/examples/quest_shape_in_memory.cpp
+++ src/axom/quest/examples/quest_shape_in_memory.cpp
@@ -37,12 +37,6 @@
   #include "mpi.h"
 #endif
 
-// RAJA
-#if !defined(AXOM_USE_RAJA)
-  #error quest_shape_in_memory example and IntersectionShaper require RAJA
-#endif
-#include "RAJA/RAJA.hpp"
-
 // C/C++ includes
 #include <string>
 #include <vector>
@@ -1321,8 +1315,7 @@ double sumMaterialVolumes(sidre::MFEMSidreDataCollection* dc, const std::string&
                                      axom::execution_space<ExecSpace>::allocatorID());
   auto volFracView = volFracGfArray.view();
 
-  using ReducePolicy = typename axom::execution_space<ExecSpace>::reduce_policy;
-  RAJA::ReduceSum<ReducePolicy, double> localVol(0);
+  axom::ReduceSum<ExecSpace, double> localVol(0);
   axom::for_all<ExecSpace>(
     cellCount,
     AXOM_LAMBDA(axom::IndexType i) { localVol += volFracView[i] * elementVolsView[i]; });
@@ -1364,8 +1357,7 @@ double sumMaterialVolumesImpl(sidre::Group* meshGrp, const std::string& material
   axom::sidre::View* volFrac = meshGrp->getView(vfFieldValuesPath);
   axom::ArrayView<double> volFracView(volFrac->getArray(), cellCount);
 
-  using ReducePolicy = typename axom::execution_space<ExecSpace>::reduce_policy;
-  RAJA::ReduceSum<ReducePolicy, double> localVol(0);
+  axom::ReduceSum<ExecSpace, double> localVol(0);
   axom::for_all<ExecSpace>(
     cellCount,
     AXOM_LAMBDA(axom::IndexType i) { localVol += volFracView[i] * elementVolsView[i]; });
@@ -1886,12 +1878,11 @@ int main(int argc, char** argv)
 
   // For error checking, work on host.
   using ExecSpace = axom::SEQ_EXEC;
-  using ReducePolicy = typename axom::execution_space<ExecSpace>::reduce_policy;
 
   //---------------------------------------------------------------------------
   // Correctness test: volume fractions should be in [0,1].
   //---------------------------------------------------------------------------
-  RAJA::ReduceSum<ReducePolicy, axom::IndexType> rangeViolationCount(0);
+  axom::ReduceSum<ExecSpace, axom::IndexType> rangeViolationCount(0);
   for(const auto& vfName : allVfNames)
   {
     std::string fieldName = "vol_frac_" + vfName;
@@ -1919,7 +1910,7 @@ int main(int argc, char** argv)
   axom::Array<double> volSums(cellCount);
   volSums.fill(0.0);
   axom::ArrayView<double> volSumsView = volSums.view();
-  RAJA::ReduceSum<ReducePolicy, axom::IndexType> nonUnitSums(0);
+  axom::ReduceSum<ExecSpace, axom::IndexType> nonUnitSums(0);
   for(const auto& vfName : allVfNames)
   {
     std::string fieldName = "vol_frac_" + vfName;
--- src/axom/quest/examples/shaping_driver.cpp
+++ src/axom/quest/examples/shaping_driver.cpp
@@ -32,11 +32,6 @@
   #include "mpi.h"
 #endif
 
-// RAJA
-#ifdef AXOM_USE_RAJA
-  #include "RAJA/RAJA.hpp"
-#endif
-
 // C/C++ includes
 #include <string>
 #include <vector>
@@ -304,18 +299,16 @@ public:
 
       std::stringstream pol_sstr;
       pol_sstr << "Set runtime policy for intersection-based sampling method.";
-#if defined(AXOM_USE_RAJA) && defined(AXOM_USE_UMPIRE)
-      pol_sstr << "\nSet to 'seq' or 0 to use the RAJA sequential policy.";
-  #ifdef AXOM_USE_OPENMP
+      pol_sstr << "\nSet to 'seq' or 0 to use the sequential policy.";
+  #if defined(AXOM_USE_RAJA) && defined(AXOM_USE_OPENMP)
       pol_sstr << "\nSet to 'omp' or 1 to use the RAJA OpenMP policy.";
   #endif
-  #ifdef AXOM_USE_CUDA
+  #if defined(AXOM_USE_RAJA) && defined(AXOM_USE_UMPIRE) && defined(AXOM_USE_CUDA)
       pol_sstr << "\nSet to 'cuda' or 2 to use the RAJA CUDA policy.";
   #endif
-  #ifdef AXOM_USE_HIP
+  #if defined(AXOM_USE_RAJA) && defined(AXOM_USE_UMPIRE) && defined(AXOM_USE_HIP)
       pol_sstr << "\nSet to 'hip' or 3 to use the RAJA HIP policy.";
   #endif
-#endif
 
       intersection_options->add_option("-p, --policy", policy, pol_sstr.str())
         ->capture_default_str()
@@ -538,14 +531,10 @@ int main(int argc, char** argv)
     shaper = new quest::SamplingShaper(params.shapeSet, &shapingDC);
     break;
   case ShapingMethod::Intersection:
-#if defined(AXOM_USE_RAJA) && defined(AXOM_USE_UMPIRE)
     shaper = new quest::IntersectionShaper(params.policy,
                                            axom::policyToDefaultAllocatorID(params.policy),
                                            params.shapeSet,
                                            &shapingDC);
-#else
-    SLIC_ERROR("IntersectionShaper requires Axom to be configured with Umpire.");
-#endif
     break;
   }
   SLIC_ASSERT_MSG(shaper != nullptr, "Invalid shaping method selected!");
@@ -583,7 +572,6 @@ int main(int argc, char** argv)
     }
   }
 
-#if defined(AXOM_USE_RAJA) && defined(AXOM_USE_UMPIRE)
   // Set specific parameters here for IntersectionShaper
   if(auto* intersectionShaper = dynamic_cast<quest::IntersectionShaper*>(shaper))
   {
@@ -594,7 +582,6 @@ int main(int argc, char** argv)
       intersectionShaper->setFreeMaterialName(params.backgroundMaterial);
     }
   }
-#endif
 
   //---------------------------------------------------------------------------
   // Project initial volume fractions, if applicable
