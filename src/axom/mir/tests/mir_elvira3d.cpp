af40762a3644e7081ce2942db4ea54132cdb2465
Brian Manh Hien Han <han12@llnl.gov>
2025-07-25 11:22:32 -0700

    Fix for mir_elvira3d test

--- src/axom/mir/tests/mir_elvira3d.cpp
+++ src/axom/mir/tests/mir_elvira3d.cpp
@@ -313,7 +313,7 @@ struct test_Elvira3D
         for(axom::IndexType i = 0; i < ids.size(); i++)
         {
           auto index = axom::utilities::binary_search(sortedIdsView, ids[i]);
-          SLIC_ASSERT(index >= 0 && index < nmats);
+          SLIC_ASSERT(index >= 0 && index < sortedIdsView.size());
 
           // Use an atomic to sum the value.
           axom::atomicAdd<ExecSpace>(totalVolumeView.data() + index, zoneVolumes[zi] * vfs[i]);
