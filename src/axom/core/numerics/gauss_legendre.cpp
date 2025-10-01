67fdfc68724ba8d6239975ccf3d6d75f8f5dc323
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2025-10-01 09:12:23 -0700

    Add test

--- src/axom/core/numerics/gauss_legendre.cpp
+++ src/axom/core/numerics/gauss_legendre.cpp
@@ -36,43 +36,55 @@ void compute_rule(int np, double* nodes, double* weights)
   // Nodes are mirrored across x = 0.5, so only need to evaluate half
   for(int i = 1; i <= m; ++i)
   {
+    // Each node is the root of a Legendre polynomial, 
+    //  which are approximately uniformly distirbuted in arccos(xi).
+    // This makes cos a good initial guess for subsequent Newton iterations
     double z = std::cos(M_PI * (i - 0.25) / (n + 0.5));
-    double pp, p1, dz, xi = 0.0;
+    double Pp_n, P_n, dz, xi = 0.0;
 
     bool done = false;
     while(true)
     {
-      double p2 = 1.0;
-      p1 = z;
+      // Recursively evaluate P_n(z) through the recurrence relation
+      //  n * P_n(z) = (2n - 1) * P_{n-1}(z) - (n - 1) * P_{n - 2}(z)
+      double P_nm1 = 1.0;   // P_0(z) = 1
+      P_n = z;              // P_1(z) = z
       for(int j = 2; j <= n; ++j)
       {
-        double p3 = p2;
-        p2 = p1;
-        p1 = ((2 * j - 1) * z * p2 - (j - 1) * p3) / j;
+        double P_nm2 = P_nm1;
+        P_nm1 = P_n;
+        P_n = ((2 * j - 1) * z * P_nm1 - (j - 1) * P_nm2) / j;
       }
-      // p1 is Legendre polynomial
 
-      pp = n * (z * p1 - p2) / (z * z - 1);
+      // Evaluate P'_n(z) using another recurrence relation
+      //  (z^2 - 1) * P'_n(z) = n * z * P_n(z) - n * P_{n-1}(z)
+      Pp_n = n * (z * P_n - P_nm1) / (z * z - 1);
 
       if(done)
       {
         break;
       }
 
-      dz = p1 / pp;
+      // Compute the Newton method step size
+      dz = P_n / Pp_n;
 
       if(std::fabs(dz) < 1e-16)
       {
         done = true;
+
+        // Scale back to [0, 1]
         xi = ((1 - z) + dz) / 2;
       }
 
+      // Take the Newton step, repeat the process
       z -= dz;
     }
 
     nodes[i - 1] = xi;
     nodes[n - i] = 1.0 - xi;
-    weights[i - 1] = weights[n - i] = 1.0 / (4.0 * xi * (1.0 - xi) * pp * pp);
+
+    // Evaluate the weight as w_i = 2 / (1 - z^2) / P'_n(z)^2, with z \in [-1, 1]
+    weights[i - 1] = weights[n - i] = 1.0 / (4.0 * xi * (1.0 - xi) * Pp_n * Pp_n);
   }
 }
 } /* end namespace numerics */
--- src/axom/core/tests/numerics_quadrature.hpp
+++ src/axom/core/tests/numerics_quadrature.hpp
@@ -6,18 +6,58 @@
 #include "gtest/gtest.h"
 
 #include "axom/core/numerics/gauss_legendre.hpp"
+#include <iomanip>
 
 TEST(numerics_quadrature, generate_rules)
 {
-  const int N = 3;
+  const int N = 50;
 
   double nodes[N];
   double weights[N];
 
-  axom::numerics::compute_rule(N, nodes, weights);
-
-  for(int i = 0; i < N; ++i)
+  // Define a collection of random coefficients for a polynomial
+  double coeffs[2 * N - 1];
+  for(int j = 0; j < 2 * N - 1; ++j)
   {
-    std::cout << nodes[i] << " " << weights[i] << std::endl;
+    coeffs[j] = axom::utilities::random_real(-5.0, 5.0);
+    std::cout << coeffs[j] << std::endl;
+  }
+
+  // Test that the rules provide exact integration for polynomials of degree 2n - 1
+  for(int npts = 1; npts < N; ++npts)
+  {
+    int degree = 2 * npts - 1;
+
+    // Evaluate the area under the curve from 0 to 1 analytically
+    double analytic_result = 0.0;
+    for(int j = 0; j < degree; ++j)
+    {
+      analytic_result += coeffs[j] / (j + 1);
+    }
+
+    // Evaluate using the quadrature rule
+    axom::numerics::compute_rule(npts, nodes, weights);
+
+    // Evaluate the polynomial using Horner's rule
+    auto eval_polynomial = [&degree, &coeffs](double x) {
+      double result = coeffs[degree - 1];
+      for(int i = degree - 2; i >= 0; --i)
+      {
+        result = result * x + coeffs[i];
+      }
+      return result;
+    };
+
+    double quadrature_result = 0.0;
+    for(int j = 0; j < npts; ++j)
+    {      
+      quadrature_result += weights[j] * eval_polynomial(nodes[j]);
+    }
+
+    std::cout << std::setprecision(20);
+    std::cout << "=====" << std::endl;
+    std::cout << analytic_result << std::endl;
+    std::cout << quadrature_result << std::endl;
+    std::cout << "=====" << std::endl;
   }
 }
