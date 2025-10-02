fe2358086e105f2d4df575c566bdb1817e116b61
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2025-10-01 18:18:23 -0700

    Update comments, test

--- src/axom/core/numerics/quadrature.hpp
+++ src/axom/core/numerics/quadrature.hpp
@@ -3,15 +3,15 @@
 //
 // SPDX-License-Identifier: (BSD-3-Clause)
 
-#ifndef AXOM_NUMERICS_GAUSS_LEGENDGRE_HPP_
-#define AXOM_NUMERICS_GAUSS_LEGENDGRE_HPP_
+#ifndef AXOM_NUMERICS_QUADRATURE_HPP_
+#define AXOM_NUMERICS_QUADRATURE_HPP_
 
 #include "axom/core/Array.hpp"
 
 /*!
- * \file gauss_legendre.hpp
+ * \file quadrature.hpp
  * The functions declared in this header file find the nodes and weights of 
- *   arbitrary order Gauss-Legendre quadrature rules
+ *   arbitrary order quadrature rules
  */
 
 namespace axom
@@ -19,7 +19,11 @@ namespace axom
 namespace numerics
 {
 
-//! \brief Class to store quadature nodes and weights
+/*!
+ * \class QuadratureRule
+ *
+ * \brief Stores a fixed array of 1D quadrature points and weights
+ */
 class QuadratureRule
 {
   // Define friend functions so rules can only be created via compute_rule() methods
@@ -28,21 +32,51 @@ class QuadratureRule
 public:
   QuadratureRule() = default;
 
+  //! \brief Accessor for quadrature nodes
   double node(size_t idx) const { return m_nodes[idx]; };
+
+  //! \brief Accessor for quadrature weights
   double weight(size_t idx) const { return m_weights[idx]; };
 
 private:
+  //! \brief Private constructor for use in compute_<rule>() methods. Only allocates memory
   QuadratureRule(int npts) : m_nodes(npts), m_weights(npts) { };
 
   axom::Array<double> m_nodes;
   axom::Array<double> m_weights;
 };
 
+/*!
+ * \brief Computes a 1D quadrature rule of Gauss-Legendre points 
+ *
+ * \param [in] npts The number of points in the rule
+ * 
+ * A Gauss-Legendre rule with \a npts points can exactly integrate
+ *  polynomials of order 2 * npts - 1
+ *
+ * \note This method constructs the points by scratch each time, without caching
+ * \sa get_gauss_legendre(int)
+ *
+ * \return The `QuadratureRule` object which contains axom::Array<double>'s of nodes and weights
+ */
 QuadratureRule compute_gauss_legendre(int npts);
 
+/*!
+ * \brief Computes or accesses a precomputed 1D quadrature rule of Gauss-Legendre points 
+ *
+ * \param [in] npts The number of points in the rule
+ * 
+ * A Gauss-Legendre rule with \a npts points can exactly integrate
+ *  polynomials of order 2 * npts - 1
+ *
+ * \note If this method has already been called for a given order, it will reuse the same quadrature points
+ *  without needing to recompute them
+ *
+ * \return The `QuadratureRule` object which contains axom::Array<double>'s of nodes and weights
+ */
 const QuadratureRule& get_gauss_legendre(int npts);
 
 } /* end namespace numerics */
 } /* end namespace axom */
 
-#endif  // AXOM_NUMERICS_GAUSS_LEGENDGRE_HPP_
+#endif  // AXOM_NUMERICS_QUADRATURE_HPP_
--- src/axom/core/tests/numerics_quadrature.hpp
+++ src/axom/core/tests/numerics_quadrature.hpp
@@ -8,21 +8,25 @@
 #include "axom/core/numerics/quadrature.hpp"
 #include <iomanip>
 
-TEST(numerics_quadrature, generate_rules)
+TEST(numerics_quadrature, gauss_legendre_math_check)
 {
-  const int N = 50;
+  const int N = 200;
 
-  // Define a collection of random coefficients for a polynomial
   double coeffs[2 * N - 1];
-  for(int j = 0; j < 2 * N - 1; ++j)
-  {
-    coeffs[j] = axom::utilities::random_real(-5.0, 5.0);
-  }
 
   // Test that the rules provide exact integration for polynomials of degree 2n - 1
   for(int npts = 1; npts < N; ++npts)
   {
+    // Evaluate using the quadrature rule
+    auto rule = axom::numerics::get_gauss_legendre(npts);
     int degree = 2 * npts - 1;
+
+    // Define a collection of random coefficients for a polynomial
+    for(int j = 0; j < degree; ++j)
+    {
+      // Seed the random coefficients for consistency in the test
+      coeffs[j] = axom::utilities::random_real(-1.0, 1.0, npts + j);
+    }
 
     // Evaluate the area under the curve from 0 to 1 analytically
     double analytic_result = 0.0;
@@ -30,9 +34,6 @@ TEST(numerics_quadrature, generate_rules)
     {
       analytic_result += coeffs[j] / (j + 1);
     }
-
-    // Evaluate using the quadrature rule
-    auto rule = axom::numerics::compute_gauss_legendre(npts);
 
     // Evaluate the polynomial using Horner's rule
     auto eval_polynomial = [&degree, &coeffs](double x) {
@@ -50,10 +51,54 @@ TEST(numerics_quadrature, generate_rules)
       quadrature_result += rule.weight(j) * eval_polynomial(rule.node(j));
     }
 
-    std::cout << std::setprecision(20);
-    std::cout << "=====" << std::endl;
-    std::cout << analytic_result << std::endl;
-    std::cout << quadrature_result << std::endl;
-    std::cout << "=====" << std::endl;
+    EXPECT_NEAR(quadrature_result, analytic_result, 1e-10);
+
+    // Check that nodes aren't duplicated
+    for(int j = 1; j < npts; ++j)
+    {
+      EXPECT_GT(rule.node(j), rule.node(j - 1));
+    }
+
+    // Check that the sum of the weights is 1, and that all are positive
+    double weight_sum = 0.0;
+    for(int j = 0; j < npts; ++j)
+    {
+      EXPECT_GT(rule.weight(j), 0.0);
+      weight_sum += rule.weight(j);
+    }
+
+    EXPECT_NEAR(weight_sum, 1.0, 1e-10);
+
+    // Check that each node is the root of the next Legendre polynomial
+    for(int j = 0; j < npts; ++j)
+    {
+      // Rescale the node to [-1, 1]
+      double z = 2 * rule.node(j) - 1;
+
+      double P_n = z, P_nm1 = 1.0;
+      for(int k = 2; k <= npts; ++k)
+      {
+        double P_nm2 = P_nm1;
+        P_nm1 = P_n;
+        P_n = ((2 * k - 1) * z * P_nm1 - (k - 1) * P_nm2) / k;
+      }
+
+      // Note that this metric does not directly imply that |z - z*| < tol
+      EXPECT_NEAR(P_n, 0.0, 1e-10);
+    }
   }
+}
+
+TEST(numerics_quadrature, gauss_legendre_cache_check)
+{
+  // The first two rules are put in the cache
+  const axom::numerics::QuadratureRule& first_rule = axom::numerics::get_gauss_legendre(20);
+  const axom::numerics::QuadratureRule& second_rule = axom::numerics::get_gauss_legendre(20);
+
+  // The third is not
+  const axom::numerics::QuadratureRule& third_rule = axom::numerics::compute_gauss_legendre(20);
+
+  // Check that the two rules are located in the same place in memory, and the third isn't
+  EXPECT_EQ(&first_rule, &second_rule);
+  EXPECT_NE(&first_rule, &third_rule);
 }
