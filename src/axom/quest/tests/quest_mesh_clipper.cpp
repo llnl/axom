398ab74c8755aedf9706dc373cec1ffe3158d53c
Brian T.N. Gunney <gunney1@llnl.gov>
2026-01-07 11:59:10 -0800

    Remove obsolete test code.

--- src/axom/quest/tests/quest_mesh_clipper.cpp
+++ src/axom/quest/tests/quest_mesh_clipper.cpp
@@ -506,7 +506,6 @@ axom::klee::Geometry createGeom_Sor(const std::string& geomName)
   double zLen = 0.5 * (params.length < 0 ? 2.40 : params.length);
   double maxR = params.radius < 0 ? 1.10 : params.radius;
   axom::Array<Point2DType> discretePts(0, 10);
-#if 1
   discretePts.push_back(Point2DType({-1.0 * zLen, 1.0 * maxR}));
   discretePts.push_back(Point2DType({0.4 * zLen, 1.0 * maxR}));
   discretePts.push_back(Point2DType({0.4 * zLen, 0.7 * maxR}));
@@ -519,15 +518,6 @@ axom::klee::Geometry createGeom_Sor(const std::string& geomName)
   discretePts.push_back(Point2DType({0.2 * zLen, 0.5 * maxR}));
   discretePts.push_back(Point2DType({0.2 * zLen, 0.7 * maxR}));
   discretePts.push_back(Point2DType({-1.0 * zLen, 0.7 * maxR}));
-#else
-  discretePts.push_back(Point2DType({-1.0 * zLen, 0.4 * maxR}));
-  discretePts.push_back(Point2DType({0.0 * zLen, 1.0 * maxR}));
-  discretePts.push_back(Point2DType({0.6 * zLen, 1.0 * maxR}));
-  discretePts.push_back(Point2DType({1.0 * zLen, 0.8 * maxR}));
-  discretePts.push_back(Point2DType({1.0 * zLen, 0.6 * maxR}));
-  discretePts.push_back(Point2DType({0.2 * zLen, 0.4 * maxR}));
-  discretePts.push_back(Point2DType({0.0 * zLen, 0.0 * maxR}));
-#endif
   axom::ArrayView<const double, 2> discreteFunction((const double*)discretePts.data(),
                                                     discretePts.size(),
                                                     2);
