e2c233b1ffbed43dd478c3ee5779b0a1b8903939
Kenneth Weiss <weiss27@llnl.gov>
2026-01-28 20:13:00 -0800

    Fixes guards for MFEMReader -- it does not require Sidre

--- src/axom/quest/io/MFEMReader.hpp
+++ src/axom/quest/io/MFEMReader.hpp
@@ -9,8 +9,8 @@
 
 #include "axom/config.hpp"
 
-#if !defined(AXOM_USE_MFEM) || !defined(AXOM_USE_SIDRE)
-  #error MFEMReader should only be included when Axom is configured with MFEM and Sidre
+#ifndef AXOM_USE_MFEM
+  #error MFEMReader should only be included when Axom is configured with MFEM
 #endif
 
 #include "axom/core/Array.hpp"
