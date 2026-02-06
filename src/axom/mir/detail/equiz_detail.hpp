90718b43c82311217a9e13e284c12027ebd85715
Brad Whitlock <whitlock2@llnl.gov>
2026-02-06 13:24:15 -0800

    Use inBounds_0_N in some asserts.

--- src/axom/mir/detail/equiz_detail.hpp
+++ src/axom/mir/detail/equiz_detail.hpp
@@ -79,7 +79,7 @@ public:
       {
         const auto nid = nodeIdsView[i];
         SLIC_ASSERT_MSG(
-          nid >= 0 && nid < static_cast<ConnectivityType>(m_matvfViews[0].size()),
+          axom::utilities::inBounds_0_N(nid, static_cast<ConnectivityType>(m_matvfViews[0].size())),
           axom::fmt::format("Node id {} is not in range [0, {}).", nid, m_matvfViews[0].size()));
 
         // clang-format off
@@ -107,10 +107,10 @@ public:
       if(zoneMatID != NULL_MATERIAL) backgroundIndex = matNumberToIndex(zoneMatID);
       // Determine the matvf view index for the current material.
       SLIC_ASSERT_MSG(
-        id0 >= 0 && id0 < static_cast<ConnectivityType>(m_matvfViews[0].size()),
+        axom::utilities::inBounds_0_N(id0, static_cast<ConnectivityType>(m_matvfViews[0].size())),
         axom::fmt::format("Node id {} is not in range [0, {}).", id0, m_matvfViews[0].size()));
       SLIC_ASSERT_MSG(
-        id1 >= 0 && id1 < static_cast<ConnectivityType>(m_matvfViews[0].size()),
+        axom::utilities::inBounds_0_N(id1, static_cast<ConnectivityType>(m_matvfViews[0].size())),
         axom::fmt::format("Node id {} is not in range [0, {}).", id1, m_matvfViews[0].size()));
 
       // Get the volume fractions for mat1, mat2 at the edge endpoints id0, id1.
