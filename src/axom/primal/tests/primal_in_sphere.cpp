1a67feeca59d0c16290a6fd932b09de1178a22ef
Kenneth Weiss <weiss27@llnl.gov>
2026-03-19 20:39:51 -0700

    Refactors primal's orientation and in_sphere predicates
    These are all based on helper functions that return the orientation/in_sphere determinant.
    We added variants of these primitives in the primal::robust namespace that return
    primal::OrientationResult (ON_POSITIVE_SIDE, ON_NEGATIVE_SIDE and ON_BOUNDARY)
    to defer the classification decision to a caller.
    
    This allows for cleaning up the implementation in the Delaunay class.

--- src/axom/primal/tests/primal_in_sphere.cpp
+++ src/axom/primal/tests/primal_in_sphere.cpp
@@ -39,11 +39,15 @@ TEST(primal_in_sphere, test_in_sphere_2d)
     PointType q1 {0.1, 0.1};
     EXPECT_TRUE(in_sphere(q1, p0, p1, p2));
     EXPECT_TRUE(in_sphere(q1, tri));
+    EXPECT_EQ(primal::ON_NEGATIVE_SIDE, primal::robust::in_sphere(q1, p0, p1, p2));
+    EXPECT_EQ(primal::ON_NEGATIVE_SIDE, primal::robust::in_sphere(q1, tri));
 
     // outside triangle
     PointType q2 {0.78, 0.6};
     EXPECT_TRUE(in_sphere(q2, p0, p1, p2));
     EXPECT_TRUE(in_sphere(q2, tri));
+    EXPECT_EQ(primal::ON_NEGATIVE_SIDE, primal::robust::in_sphere(q2, p0, p1, p2));
+    EXPECT_EQ(primal::ON_NEGATIVE_SIDE, primal::robust::in_sphere(q2, tri));
   }
 
   // Test some points that are on the circumcircle
@@ -53,12 +57,16 @@ TEST(primal_in_sphere, test_in_sphere_2d)
     EXPECT_FALSE(in_sphere(q1, tri));
     EXPECT_TRUE(in_sphere(q1, p0, p1, p2, 1e-8, true));
     EXPECT_TRUE(in_sphere(q1, tri, 1e-8, true));
+    EXPECT_EQ(primal::ON_BOUNDARY, primal::robust::in_sphere(q1, p0, p1, p2, 1e-8));
+    EXPECT_EQ(primal::ON_BOUNDARY, primal::robust::in_sphere(q1, tri, 1e-8));
 
     PointType q2 {0, 1};
     EXPECT_FALSE(in_sphere(q2, p0, p1, p2));
     EXPECT_FALSE(in_sphere(q2, tri));
     EXPECT_TRUE(in_sphere(q2, p0, p1, p2, 1e-8, true));
     EXPECT_TRUE(in_sphere(q2, tri, 1e-8, true));
+    EXPECT_EQ(primal::ON_BOUNDARY, primal::robust::in_sphere(q2, p0, p1, p2, 1e-8));
+    EXPECT_EQ(primal::ON_BOUNDARY, primal::robust::in_sphere(q2, tri, 1e-8));
   }
 
   // Test some points that are outside the circumcircle
@@ -66,10 +74,12 @@ TEST(primal_in_sphere, test_in_sphere_2d)
     PointType q1 {1.1, 0};
     EXPECT_FALSE(in_sphere(q1, p0, p1, p2));
     EXPECT_FALSE(in_sphere(q1, tri));
+    EXPECT_EQ(primal::ON_POSITIVE_SIDE, primal::robust::in_sphere(q1, p0, p1, p2));
 
     PointType q2 {-5, -10};
     EXPECT_FALSE(in_sphere(q2, p0, p1, p2));
     EXPECT_FALSE(in_sphere(q2, tri));
+    EXPECT_EQ(primal::ON_POSITIVE_SIDE, primal::robust::in_sphere(q2, p0, p1, p2));
   }
 }
 
@@ -91,10 +101,14 @@ TEST(primal_in_sphere, test_in_sphere_3d)
     PointType q1 {0.5, 0.5, 0.5};
     EXPECT_TRUE(in_sphere(q1, p0, p1, p2, p3));
     EXPECT_TRUE(in_sphere(q1, tet));
+    EXPECT_EQ(primal::ON_NEGATIVE_SIDE, primal::robust::in_sphere(q1, p0, p1, p2, p3));
+    EXPECT_EQ(primal::ON_NEGATIVE_SIDE, primal::robust::in_sphere(q1, tet));
 
     PointType q2 {0., 0., 0.};
     EXPECT_TRUE(in_sphere(q2, p0, p1, p2, p3));
     EXPECT_TRUE(in_sphere(q2, tet));
+    EXPECT_EQ(primal::ON_NEGATIVE_SIDE, primal::robust::in_sphere(q2, p0, p1, p2, p3));
+    EXPECT_EQ(primal::ON_NEGATIVE_SIDE, primal::robust::in_sphere(q2, tet));
   }
 
   // Test some points that are on the circumsphere
@@ -104,12 +118,16 @@ TEST(primal_in_sphere, test_in_sphere_3d)
     EXPECT_FALSE(in_sphere(q1, tet));
     EXPECT_TRUE(in_sphere(q1, p0, p1, p2, p3, 1e-8, true));
     EXPECT_TRUE(in_sphere(q1, tet, 1e-8, true));
+    EXPECT_EQ(primal::ON_BOUNDARY, primal::robust::in_sphere(q1, p0, p1, p2, p3, 1e-8));
+    EXPECT_EQ(primal::ON_BOUNDARY, primal::robust::in_sphere(q1, tet, 1e-8));
 
     PointType q2 {-1, 1, 1};
     EXPECT_FALSE(in_sphere(q2, p0, p1, p2, p3));
     EXPECT_FALSE(in_sphere(q2, tet));
     EXPECT_TRUE(in_sphere(q2, p0, p1, p2, p3, 1e-8, true));
     EXPECT_TRUE(in_sphere(q2, tet, 1e-8, true));
+    EXPECT_EQ(primal::ON_BOUNDARY, primal::robust::in_sphere(q2, p0, p1, p2, p3, 1e-8));
+    EXPECT_EQ(primal::ON_BOUNDARY, primal::robust::in_sphere(q2, tet, 1e-8));
   }
 
   // Test some points that are outside the circumsphere
@@ -117,10 +135,12 @@ TEST(primal_in_sphere, test_in_sphere_3d)
     PointType q1 {1.1, 1, 1};
     EXPECT_FALSE(in_sphere(q1, p0, p1, p2, p3));
     EXPECT_FALSE(in_sphere(q1, tet));
+    EXPECT_EQ(primal::ON_POSITIVE_SIDE, primal::robust::in_sphere(q1, p0, p1, p2, p3));
 
     PointType q2 {-1.1, 1, 1};
     EXPECT_FALSE(in_sphere(q2, p0, p1, p2, p3));
     EXPECT_FALSE(in_sphere(q2, tet));
+    EXPECT_EQ(primal::ON_POSITIVE_SIDE, primal::robust::in_sphere(q2, p0, p1, p2, p3));
   }
 }
 
