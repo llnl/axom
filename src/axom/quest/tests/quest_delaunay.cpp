f380aa167eb434c35240aaa1689dcbb7ef1758c5
Kenneth Weiss <weiss27@llnl.gov>
2026-04-14 21:22:34 -0700

    Adds regression tests for 2D and 3D invalid Delaunay configurations

--- src/axom/quest/tests/quest_delaunay.cpp
+++ src/axom/quest/tests/quest_delaunay.cpp
@@ -57,7 +57,95 @@ void expectConformingMesh(const DelaunayType<DIM>& dt)
   EXPECT_TRUE(dt.isConforming(true));
 }
 
+template <int DIM>
+void expectValidDelaunayWithBoundary(const DelaunayType<DIM>& dt)
+{
+  EXPECT_TRUE(dt.getMeshData()->isValid(true));
+  EXPECT_TRUE(dt.getMeshData()->isConforming(true));
+  EXPECT_TRUE(dt.isConforming(true));
+  EXPECT_TRUE(dt.isValid(true));
+}
+
 }  // namespace
+
+TEST(quest_delaunay, local_bad_geometry_pool_2d)
+{
+  using PointType = typename DelaunayType<2>::PointType;
+  using BoundingBox = typename DelaunayType<2>::BoundingBox;
+  using ValidationMode = typename DelaunayType<2>::InsertionValidationMode;
+
+  const std::vector<PointType> points {
+    PointType {0.72835559683934026, 0.82937230204996126},
+    PointType {0.62600920572645069, 0.84649465468911222},
+    PointType {0.057066612786023117, 0.98518528899105051},
+    PointType {0.50420652587117898, 0.88131216460192052},
+    PointType {0.64623860530040367, 0.84784930386894053},
+    PointType {0.63047324496924584, 0.8517482773644065},
+    PointType {0.61321189052614333, 0.8508361023097063},
+    PointType {0.6153686267133367, 0.85060225303567505},
+    PointType {0.57633479792557119, 0.86309485101808103},
+    PointType {0.5734747290517247, 0.85999079345999363},
+    PointType {0.59008692995299927, 0.85528988587964816},
+    PointType {0.59200361573061988, 0.86005025488638098},
+    PointType {0.60412589378177151, 0.85608666941414724},
+    PointType {0.59008239905455229, 0.86082490771226516},
+    PointType {0.59108093308168341, 0.85843584821320762},
+    PointType {0.63956447880378053, 0.849279316704322},
+    PointType {0.55233739946809535, 0.86640226672486664},
+  };
+
+  DelaunayType<2> dt;
+  dt.initializeBoundary(BoundingBox(PointType {0., 0.}, PointType {1., 1.}));
+  dt.setInsertionValidationMode(ValidationMode::Local);
+  insertPoints(dt, std::vector<PointType>(points.begin(), points.end() - 1));
+  expectValidDelaunayWithBoundary(dt);
+
+  dt.insertPoint(points.back());
+  expectValidDelaunayWithBoundary(dt);
+}
+
+TEST(quest_delaunay, local_bad_geometry_pool_3d)
+{
+  using PointType = typename DelaunayType<3>::PointType;
+  using BoundingBox = typename DelaunayType<3>::BoundingBox;
+
+  const std::vector<PointType> points {
+    PointType {0.51599885628789743, 0.75470155608398004, 0.40855633126704233},
+    PointType {0.50343513905278869, 0.76256644865428691, 0.41550002353843529},
+    PointType {0.52181994461717562, 0.75507132048650449, 0.40434771160709904},
+    PointType {0.51222305794382439, 0.76931259688762987, 0.41173021886913774},
+    PointType {0.51893390889491309, 0.7608695730433962, 0.39023418626911949},
+    PointType {0.49870963994503914, 0.73975835990074734, 0.40518385970598131},
+    PointType {0.52105735259915775, 0.77213020101602725, 0.40722478047503557},
+    PointType {0.49002686184837962, 0.75462235742107142, 0.41591466571185198},
+    PointType {0.50501491031893275, 0.76598310556724625, 0.39817919534292223},
+    PointType {0.48943926088386502, 0.75900280017153021, 0.40203090542081277},
+    PointType {0.5113764391413268, 0.76710422391570976, 0.40773506111442864},
+    PointType {0.51178826597709415, 0.77818416231550269, 0.3925790516462227},
+    PointType {0.52301040315781211, 0.77096219982230552, 0.40733482938935728},
+    PointType {0.53346303899079406, 0.77993659887621847, 0.39369144158113362},
+    PointType {0.50029750641338455, 0.76954990769049825, 0.39799982472078405},
+    PointType {0.52770121131451853, 0.76483515658193735, 0.40353420628419612},
+    PointType {0.50952730554172287, 0.74599842639434555, 0.41708835434555841},
+    PointType {0.52956607163853775, 0.75418313145478433, 0.40980177397908779},
+    PointType {0.51225516061546172, 0.75892162524570184, 0.38822059123401836},
+    PointType {0.51473015232418118, 0.74851193285704143, 0.39787104734305007},
+    PointType {0.51352196009694051, 0.76305382863756566, 0.39295743334708788},
+    PointType {0.51822520982029341, 0.76176127807719429, 0.42723370773918606},
+    PointType {0.51440345815174604, 0.7705063813945725, 0.41385100134174907},
+    PointType {0.50863912619096874, 0.76214178681356526, 0.39419055286657634},
+    PointType {0.51550017071903309, 0.75494779413607704, 0.39942038195430957},
+    PointType {0.51299271231867682, 0.76122513269993519, 0.4067726415350541},
+  };
+
+  DelaunayType<3> dt;
+  dt.initializeBoundary(BoundingBox(PointType {0., 0., 0.}, PointType {1., 1., 1.}));
+  insertPoints(dt, std::vector<PointType>(points.begin(), points.end() - 1));
+  expectValidDelaunayWithBoundary(dt);
+
+  dt.insertPoint(points.back());
+  expectValidDelaunayWithBoundary(dt);
+}
 
 TEST(quest_delaunay, cocircular_square_2d)
 {
