a4f3cd6e07fd15c6ebacaaf44fe1d8fa1ab45ff4
Brad Whitlock <whitlock2@llnl.gov>
2026-05-12 16:27:26 -0700

    Run tests sequentially

--- scripts/github-actions/linux-build_and_test.sh
+++ scripts/github-actions/linux-build_and_test.sh
@@ -45,7 +45,8 @@ if [[ "$DO_BUILD" == "yes" ]] ; then
     fi
 
     echo "~~~~~~ RUNNING TESTS ~~~~~~~~"
-    make CTEST_OUTPUT_ON_FAILURE=1 test ARGS='-T Test --output-on-failure -j$NUM_BUILD_PROCS'
+    #make CTEST_OUTPUT_ON_FAILURE=1 test ARGS='-T Test --output-on-failure -j$NUM_BUILD_PROCS'
+    make CTEST_OUTPUT_ON_FAILURE=1 test ARGS='-T Test --output-on-failure'
 
     if [[ "${DO_BENCHMARKS}" == "yes" ]] ; then
         echo "~~~~~~ RUNNING BENCHMARKS ~~~~~~~~"
