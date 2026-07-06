ac10e2c32665c6de2dbcc6447f58c8b0b84c95cc
Brad Whitlock <whitlock2@llnl.gov>
2026-07-06 16:03:24 -0700

    Compatibility header

---
+++ src/axom/klee/Units.hpp
@@ -0,0 +1,32 @@
+// Copyright (c) Lawrence Livermore National Security, LLC and other
+// Axom Project Contributors. See top-level LICENSE and COPYRIGHT
+// files for dates and other details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+#ifndef AXOM_KLEE_UNITS_HPP
+#define AXOM_KLEE_UNITS_HPP
+
+#include "axom/core/utilities/Units.hpp"
+
+#include <string>
+
+namespace axom
+{
+namespace inlet
+{
+class Proxy;
+}
+namespace klee
+{
+
+using utilities::LengthUnit;
+using utilities::getLengthUnitName;
+
+LengthUnit parseLengthUnits(const std::string &unitsAsString, const std::string &path);
+
+LengthUnit parseLengthUnits(const inlet::Proxy &unitsAsProxy);
+
+}  // namespace klee
+}  // namespace axom
+
+#endif  // AXOM_KLEE_UNITS_HPP
