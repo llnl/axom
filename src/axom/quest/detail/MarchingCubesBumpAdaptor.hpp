6b26a527664065ad202402a6588c87dce3563613
Kenneth Weiss <weiss27@llnl.gov>
2026-08-31 19:42:42 -0700

    Quest: Bugfix for cuda build -- lambda capture within `if constexpr`

--- src/axom/quest/detail/MarchingCubesBumpAdaptor.hpp
+++ src/axom/quest/detail/MarchingCubesBumpAdaptor.hpp
@@ -372,6 +372,8 @@ void adaptCutFieldOutputViews(const conduit::Node& n_coords,
   axom::for_all<ExecSpace>(numNodes, [=] AXOM_HOST_DEVICE(axom::IndexType n) {
     facetNodeCoords(nodeIndexOffset + n, 0) = xView[n];
     facetNodeCoords(nodeIndexOffset + n, 1) = yView[n];
+    // Avoid first-capture in constexpr-if context error
+    (void)zView;
     if constexpr(DIM == 3)
     {
       facetNodeCoords(nodeIndexOffset + n, 2) = zView[n];
